9a49477a810be20b8c7fbdab4191ecd4e47d61fd
Sergey <Scorpion5149678@gmail.com>
2020-11-07 23:46:34 +0300

    Add methods
    AddFirst, AddByIndex, Delete, Decrease Length, Show Array

---
+++ DataStructures/ArrayList.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class ArrayList
+    {
+        public int Lenght { get; private set; }
+
+        private int[] _array;
+        private int _TrueLenght
+        {
+            get
+            {
+                return _array.Length;
+            }
+        }
+
+        public ArrayList()
+        {
+            _array = new int[9];
+            Lenght = 0;
+        }
+
+        public void Add(int value)
+        {
+            if (_TrueLenght <= Lenght)
+            {
+                IncreaseLenght();
+            }
+            _array[Lenght] = value;
+            Lenght++;
+        }
+
+        public void AddFirst(int value)
+        {
+            if (_TrueLenght <= Lenght)
+            {
+                IncreaseLenght();
+            }
+                int[] tempArray = new int[_array.Length];
+                for (int i = 0; i < Lenght; i++)
+                {
+                    tempArray[i + 1] = _array[i];
+                }
+                tempArray[0] = value;
+                _array = tempArray;
+                Lenght++;
+        }
+
+        public void AddElByIndex(int value, int index)
+        {
+            if (_TrueLenght <= Lenght)
+            {
+                IncreaseLenght();
+            }
+
+                int[] tempArray = new int[_array.Length];
+                for (int i = 0; i < index; i++)
+                {
+                    tempArray[i] = _array[i];
+                }
+                for (int i = index; i < Lenght; i++)
+                {
+                    tempArray[i + 1] = _array[i];
+                }
+                tempArray[index] = value;
+                _array = tempArray;
+                Lenght++;
+        }
+
+        public void Delete()
+        {
+            if (Lenght <= 0)
+            {
+                throw new Exception("Can't delete nothing");
+            }
+            else if(_TrueLenght > Lenght-1)
+            {
+                DecreaseLength();
+            }
+
+                int[] tempArray = new int[_array.Length];
+                for (int i = 0; i < Lenght; i++)
+                {
+                    tempArray[i] = _array[i];
+                }
+                _array = tempArray;
+                Lenght--;
+        }
+
+        public void ShowList()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                Console.Write(_array[i] + " ");
+            }
+        }
+        private void IncreaseLenght(int number = 1)
+        {
+            int newLenght = _TrueLenght;
+            while (newLenght <= Lenght + number)
+            {
+                newLenght = (int)(newLenght * 1.33 + 1);
+            }
+
+            int[] newArray = new int[newLenght];
+            Array.Copy(_array, newArray, _TrueLenght);
+            _array = newArray;
+        }
+        private void DecreaseLength(int number = 1)
+        {
+            int newLenght = _TrueLenght;
+            while (newLenght*0.75 >= Lenght)
+            {
+                newLenght = (int)(newLenght-number);
+            }
+            int[] tempArray = new int[newLenght];
+            Array.Copy(_array, tempArray, newLenght);
+            _array = tempArray;
+        }
+    }
+}
---
+++ MyDataScructConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DataStructures;
+
+namespace MyDataScructConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myList = new ArrayList();
+            myList.Add(1);
+            myList.Add(3);
+            myList.Add(6);
+            myList.Add(17);
+            myList.Add(12);
+            myList.Add(15);
+            myList.Delete();
+            myList.AddFirst(78);
+            myList.AddFirst(66);
+            myList.AddElByIndex(75, 1);
+            myList.ShowList();
+        }
+    }
+}
